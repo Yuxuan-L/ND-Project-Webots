51645aed15f5aa3f6fcb5e6c92a334dd5e898cc8
NERanger <11712605@mail.sustc.edu.cn>
2019-07-29 15:04:00 -0400

    Init commit

---
+++ controllers/Tractor_control/build/release/Tractor_control.d
@@ -0,0 +1,12 @@
+build/release/Tractor_control.o: Tractor_control.c \
+ E:\Program\Webots/include/controller/c/webots/camera.h \
+ E:\Program\Webots/include/controller/c/webots/camera_recognition_object.h \
+ E:\Program\Webots/include/controller/c/webots/types.h \
+ E:\Program\Webots/include/controller/c/webots/gps.h \
+ E:\Program\Webots/include/controller/c/webots/keyboard.h \
+ E:\Program\Webots/include/controller/c/webots/led.h \
+ E:\Program\Webots/include/controller/c/webots/lidar.h \
+ E:\Program\Webots/include/controller/c/webots/lidar_point.h \
+ E:\Program\Webots/include/controller/c/webots/motor.h \
+ E:\Program\Webots/include/controller/c/webots/robot.h \
+ E:\Program\Webots/include/controller/c/webots/nodes.h
---
+++ plugins/physics/tractor_physics/build/release/tractor_physics.d
@@ -0,0 +1,26 @@
+build/release/tractor_physics.o: tractor_physics.cpp \
+ E:\Program\Webots/include/ode/ode/ode.h \
+ E:\Program\Webots/include/ode/ode/odeconfig.h \
+ E:\Program\Webots/include/ode/ode/precision.h \
+ E:\Program\Webots/include/ode/ode/compatibility.h \
+ E:\Program\Webots/include/ode/ode/common.h \
+ E:\Program\Webots/include/ode/ode/error.h \
+ E:\Program\Webots/include/ode/ode/odeinit.h \
+ E:\Program\Webots/include/ode/ode/contact.h \
+ E:\Program\Webots/include/ode/ode/memory.h \
+ E:\Program\Webots/include/ode/ode/odemath.h \
+ E:\Program\Webots/include/ode/ode/odemath_legacy.h \
+ E:\Program\Webots/include/ode/ode/matrix.h \
+ E:\Program\Webots/include/ode/ode/timer.h \
+ E:\Program\Webots/include/ode/ode/rotation.h \
+ E:\Program\Webots/include/ode/ode/mass.h \
+ E:\Program\Webots/include/ode/ode/misc.h \
+ E:\Program\Webots/include/ode/ode/objects.h \
+ E:\Program\Webots/include/ode/ode/collision_space.h \
+ E:\Program\Webots/include/ode/ode/collision.h \
+ E:\Program\Webots/include/ode/ode/collision_trimesh.h \
+ E:\Program\Webots/include/ode/ode/export-dif.h \
+ E:\Program\Webots/include/ode/ode/version.h \
+ E:\Program\Webots/include/ode/ode/odecpp.h \
+ E:\Program\Webots/include/ode/ode/odecpp_collision.h \
+ E:\Program\Webots/include/plugins/physics.h
